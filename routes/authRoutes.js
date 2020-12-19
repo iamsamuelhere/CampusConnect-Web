ac255f52a2c159aae00e5b6f4422279f9aa36899
iamsamuelhere <iamsamuelhere@gmail.com>
2020-12-20 01:40:40 +0530

    Auth Flow Completed- alert messages

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,5 @@
 const router=require("express").Router()
+const bcrypt = require('bcrypt');
 
 //my sql
 var mysql      = require('mysql');
@@ -20,37 +21,72 @@ var connection = mysql.createConnection({
 
 //routes
 router.get("/",(req,res)=>{
-    res.render("login")
+    res.render("login",{error:0,msg:"",color:""})
 })
 
+router.post("/post-login",(req,res)=>{
+   console.log(req.body)
+  
+  connection.query('SELECT password from student where email=?',[req.body.email], function (error, results, fields) {
+    if (error) throw error;
+
+    if(results.length==0)
+    //user not registered
+    res.render("login",{error:1,msg:"User not Registered. Kindly register!",color:"danger"})
+    else{
+      HashPassDb=results[0].password
+      req.body.password;
+      bcrypt.compare(req.body.password,HashPassDb).then(function(result) {
+        if(result==true)
+        res.redirect("/home")
+        else{
+          res.render("login",{error:1,msg:"Wrong password.Login with correct credentials",color:"danger"})
+        }
+    });
+    }
+  });
+ 
+})
+
+//home
+router.get("/home",(req,res)=>{
+  res.send("success");
+})
+
+//regester routes
 router.get("/reg-form",(req,res)=>{
-    res.render("register")
+  res.render("register")
 })
-
-router.get("/db",(req,res)=>{
-    connection.query('insert into checks(name) value("pinku")', function (error, results, fields) {
-        if (error) throw error;
-        // connected!
-        console.log(results);
-      });
-})
-router.post("/post/reg",(req,res)=>{
+//regester data 
+router.post("/post-reg",(req,res)=>{
     console.log(req.body);
-    //insert into db
+    let password=req.body.password;
+    const saltRounds = 10;
+    
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+      // Store hash in your password DB.
+      if(err) throw err;
+      
+      hashed_password=hash;
+  //insert into db
+
     connection.query('insert into student SET ?',
     {usn:req.body.usn,
       name:req.body.name,
       email:req.body.email,
-      password:req.body.password,
+      password:hashed_password,
       branch:req.body.branch,
       semester:req.body.semester
     },function(error,results,fields){
       if(error) throw error;
-      //
+      //status
       console.log("inserted");
     })
 
-    res.redirect("/");
+  });
+
+//on success 
+    res.render("login",{error:1,msg:"Successfully registered!. Kindly login with the same credentials",color:"success"})
 })
 
 
